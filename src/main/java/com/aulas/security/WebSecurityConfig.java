1ad03e72bf096eac421c2ba9bd694319da9b20e1
Bastian <bastiantrabj@gmail.com>
2026-06-21 14:57:03 -0500

    Fix CORS: usar allowedOriginPatterns en lugar de allowebOrigins

--- src/main/java/com/aulas/security/WebSecurityConfig.java
+++ src/main/java/com/aulas/security/WebSecurityConfig.java
@@ -74,7 +74,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
